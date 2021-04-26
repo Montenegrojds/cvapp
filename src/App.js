56ed89f185995d020492c0697fdd9c467c77e737
monte <montenegrojds@gmail.com>
2021-04-26 07:31:47 -0500

    add <Cvbuilder /> to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
-
+import Cvbuilder from './components/cvbuilder'
 class App extends React.Component{
   render(){
-    return <h1>CvApp</h1>
+    return (
+      <div>
+        <h1>CV Builder</h1>
+        <Cvbuilder />
+      </div>
+    )
   }
 }
 export default App;
