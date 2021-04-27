3743a2594ec6c4046be4767374b66a91c6779520
monte <montenegrojds@gmail.com>
2021-04-27 10:54:39 -0500

    <Skills /> can add a list of skills

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,18 +1,113 @@
 import React from 'react';
 
 class Skills extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            addskill: '',
+            skillList: [
+                {skill: 'c++'},
+                {skill: 'python'},
+            ],
+            displayFormStatus: false,
+            skillupdate:{
+            }
+        }
+    }
+    
+    handleChanges = (e)=>{
+        const target = e.target;
+        const name = target.name;
+        const value = target.value;
+        this.setState({            
+            [name]: value        
+        });
+
+    }
+
+    displaySkillForm(){
+        this.setState({
+            displayFormStatus: true,
+        })
+        
+    }
+    onSubmitSkill = (e)=>{
+        e.preventDefault();
+        this.setState({
+            skillupdate: {
+                skill: this.state.addskill
+            },
+            displayFormStatus: false
+
+        })
+        
+        setTimeout(() => {
+            this.updatedSkillList();
+          }, 1);
+
+
+    }
+
+    updatedSkillList(){
+        this.setState({
+            skillList: this.state.skillList.concat(this.state.skillupdate)
+        })
+    }
+
+    cancelDisplayNewSkill(){
+        this.setState({
+            displayFormStatus: false,
+            addskill: '',
+
+
+        })
+    }
+
+    addSkill(){
+        if (this.state.displayFormStatus === true){
+        return(
+            <div>
+                  <form onSubmit={this.onSubmitSkill}>
+                    <label htmlFor="addskill">Add a skill:</label>
+                    <input type="text" id="addskill" 
+                        onChange={this.handleChanges}
+                        value={this.state.addSkill}
+                        name="addskill"
+                        required
+                    />
+                    <br />
+
+                    <button type="submit">Save</button> 
+                    <button onClick={()=> this.cancelDisplayNewSkill()}> cancel</button> 
+                </form>
+            </div>
+            )
+        }
+    }
+
+    displaySkillList(){
+        return this.state.skillList.map(info => {
+            return (
+                <li>{info.skill}</li>
+            )
+        })
+    }
+        
+
+
     render(){
         return(
             <div>
                 <h3>Skills</h3>
+                <button onClick={()=> this.displaySkillForm()}>
+                    +Add
+                </button>
+                {this.addSkill()}
 
-                <form>
-                    <label htmlFor="addSkill">Add a skill:</label>
-                    <input type="text" id="addSkill" />
-                    <br />
-
-                    <button>Save</button> <button> cancel</button> 
-                </form>
+                <ul>
+                    {this.displaySkillList()}
+                </ul>
+              
             </div>
         )
     }
